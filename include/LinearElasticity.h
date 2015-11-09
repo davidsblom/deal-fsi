a33166df0af753a122058eed027fc212cc7f3a6e
David Blom <davidsblom@gmail.com>
2015-11-09 16:46:23 +0100

    Cleanup header file of LinearElasticity class
    Bugfix constructor in the header

--- include/LinearElasticity.h
+++ include/LinearElasticity.h
@@ -16,36 +16,12 @@ namespace Step23
         Vector<Scalar> & b
         );
 
-
-    // @sect3{The <code>WaveEquation</code> class}
-
-    // Next comes the declaration of the main class. It's public interface of
-    // functions is like in most of the other tutorial programs. Worth
-    // mentioning is that we now have to store four matrices instead of one: the
-    // mass matrix $M$, the Laplace matrix $A$, the matrix $M+k^2\theta^2A$ used
-    // for solving for $U^n$, and a copy of the mass matrix with boundary
-    // conditions applied used for solving for $V^n$. Note that it is a bit
-    // wasteful to have an additional copy of the mass matrix around. We will
-    // discuss strategies for how to avoid this in the section on possible
-    // improvements.
-    //
-    // Likewise, we need solution vectors for $U^n,V^n$ as well as for the
-    // corresponding vectors at the previous time step, $U^{n-1},V^{n-1}$. The
-    // <code>system_rhs</code> will be used for whatever right hand side vector
-    // we have when solving one of the two linear systems in each time
-    // step. These will be solved in the two functions <code>solve_u</code> and
-    // <code>solve_v</code>.
-    //
-    // Finally, the variable <code>theta</code> is used to indicate the
-    // parameter $\theta$ that is used to define which time stepping scheme to
-    // use, as explained in the introduction. The rest is self-explanatory.
-
     template <int dim>
     class LinearElasticity
     {
 public:
 
-        WaveEquation(
+        LinearElasticity(
             double time_step,
             double theta,
             double degree,
